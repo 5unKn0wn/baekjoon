0f64fdcd3fe1b7185148dfed20b976c1edd1ced5
5unKn0wn <5unKn0wn.lee@gmail.com>
2019-03-31 21:16:48 +0900

    Create 10951.cpp

---
+++ 10951.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void)
+{
+	int a, b;
+
+	while (1) {
+		std::cin >> a >> b;
+        if (std::cin.eof())
+			break;
+		std::cout << a + b << "\n";
+	}
+
+	return 0;
+}
